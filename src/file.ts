55661782a9b85196f88492c9f0c857d17ded6e04
xvie <jansuroviak@gmail.com>
2019-05-22 13:20:11 +0200

    Handle multiple outputs

--- src/file.ts
+++ src/file.ts
@@ -1,35 +1,40 @@
-import * as fs from 'fs'
+import * as fs from 'fs';
 
 export default class StackOutputFile {
-  constructor (
-    public path: string
-  ) { }
+  constructor(public path: string | string[]) {}
 
-  public format (data: object) {
-    const ext = this.path.split('.').pop() || ''
+  public format(data: object, path: string) {
+    const ext = path.split('.').pop() || '';
 
     switch (ext.toUpperCase()) {
       case 'JSON':
-        return JSON.stringify(data, null, 2)
+        return JSON.stringify(data, null, 2);
       case 'TOML':
-        return require('tomlify-j0.4')(data, null, 0)
+        return require('tomlify-j0.4')(data, null, 0);
       case 'YAML':
       case 'YML':
-        return require('yamljs').stringify(data)
+        return require('yamljs').stringify(data);
       default:
-        throw new Error('No formatter found for `' + ext + '` extension')
+        throw new Error('No formatter found for `' + ext + '` extension');
     }
   }
 
-  public save (data: object) {
-    const content = this.format(data)
-
+  private save(data: object, path: string) {
+    const content = this.format(data, path);
     try {
-      fs.writeFileSync(this.path, content)
+      fs.writeFileSync(path, content);
     } catch (e) {
-      throw new Error('Cannot write to file: ' + this.path)
+      throw new Error('Cannot write to file: ' + this.path);
     }
 
-    return Promise.resolve()
+    return Promise.resolve();
+  }
+
+  public prepareData(data: object) {
+    if (Array.isArray(this.path)) {
+      return Promise.all(this.path.map(path => this.save(data, path)));
+    } else {
+      return this.save(data, this.path);
+    }
   }
 }
--- test/plugin.spec.ts
+++ test/plugin.spec.ts
@@ -1,83 +1,112 @@
-import sinon from 'sinon'
-
-import Plugin from '../src/plugin'
+import sinon from 'sinon';
+import Plugin from '../src/plugin';
 
 describe('Plugin', () => {
-  let providerMock = null
-  let getProvider = null
+  let providerMock = null;
+  let getProvider = null;
   const provider = {
     request: () => true,
     sdk: {
-      VERSION: '2.21.0'
-    }
-  }
+      VERSION: '2.21.0',
+    },
+  };
 
   beforeEach(() => {
-    providerMock = sinon.mock(provider)
-    getProvider = sinon.stub().returns(provider)
-  })
+    providerMock = sinon.mock(provider);
+    getProvider = sinon.stub().returns(provider);
+  });
 
   afterEach(() => {
-    providerMock.restore()
-  })
+    providerMock.restore();
+  });
 
   describe('Configuration', () => {
     it('hasHandler', () => {
       const config = {
         cli: { log: () => null },
         config: {
-          servicePath: ''
+          servicePath: '',
         },
         getProvider,
         region: 'us-east-1',
         service: {
           custom: {
             output: {
-              handler: 'foo/bar.baz'
-            }
+              handler: 'foo/bar.baz',
+            },
           },
           provider: {
-            name: 'aws'
-          }
-        }
-      }
+            name: 'aws',
+          },
+        },
+      };
 
-      const test = new Plugin(config, { serverless: true }, { options: true })
+      const test = new Plugin(config, { serverless: true });
 
-      expect(test.hasHandler()).toBe(true)
-      expect(test.hasFile()).toBe(false)
+      expect(test.hasHandler()).toBe(true);
+      expect(test.hasFile()).toBe(false);
 
-      expect(test.handler).toContain('foo/bar.baz')
-    })
-  })
+      expect(test.handler).toContain('foo/bar.baz');
+    });
+  });
 
   describe('Configuration', () => {
     it('hasFile', () => {
       const config = {
         cli: { log: () => null },
         config: {
-          servicePath: ''
+          servicePath: '',
         },
         getProvider,
         region: 'us-east-1',
         service: {
           custom: {
             output: {
-              file: 'foo/bar.toml'
-            }
+              file: 'foo/bar.toml',
+            },
           },
           provider: {
-            name: 'aws'
-          }
-        }
-      }
+            name: 'aws',
+          },
+        },
+      };
 
-      const test = new Plugin(config)
+      const test = new Plugin(config);
 
-      expect(test.hasHandler()).toBe(false)
-      expect(test.hasFile()).toBe(true)
+      expect(test.hasHandler()).toBe(false);
+      expect(test.hasFile()).toBe(true);
 
-      expect(test.file).toContain('foo/bar.toml')
-    })
-  })
-})
+      expect(test.file).toEqual('/foo/bar.toml');
+    });
+  });
+
+  describe('Configuration with array of files', () => {
+    it('hasFile', () => {
+      const config = {
+        cli: { log: () => null },
+        config: {
+          servicePath: '',
+        },
+        getProvider,
+        region: 'us-east-1',
+        service: {
+          custom: {
+            output: {
+              file: ['foo/bar.toml', 'foo/bar.json'],
+            },
+          },
+          provider: {
+            name: 'aws',
+          },
+        },
+      };
+
+      const test = new Plugin(config);
+
+      expect(test.hasHandler()).toBe(false);
+      expect(test.hasFile()).toBe(true);
+
+      expect(test.file).toEqual(['/foo/bar.toml', '/foo/bar.json']);
+    });
+  });
+});
